da671f1fee34162e3afb3870b5a5ca13f2a7a310
Rickard Natt och Dag <rickard@nattochdag.dev>
2021-06-15 07:21:55 +0200

    feat: add complete context example

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import App, { Text } from './App'
 import { screen, render } from '@testing-library/react'
+import { AppContext } from './AppContext'
 
-test.todo('renders app')
+test('renders app', () => {
+  render(<App text="Frontend testing is fun!" />)
 
-test.todo('Text using a mocked useApp hook')
+  expect(screen.getByText(/frontend testing is fun/i)).toBeInTheDocument()
+})
 
-test.todo('Text by importing the context')
+test('Text by importing the context', () => {
+  render(
+    <AppContext.Provider value={{ text: 'Frontend testing is fun' }}>
+      <Text />
+    </AppContext.Provider>
+  )
+
+  expect(screen.getByText(/frontend testing is fun/i)).toBeInTheDocument()
+})
