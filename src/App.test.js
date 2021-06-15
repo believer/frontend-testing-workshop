0987d7e65986a1de5cb60aa7878c66fdb4bf5a0c
Rickard Natt och Dag <rickard@nattochdag.dev>
2021-06-15 07:19:20 +0200

    feat: add custom hooks test

--- src/App.test.js
+++
@@ -1,6 +0,0 @@
-import App from './App'
-import { screen, render } from '@testing-library/react'
-
-test.todo('renders loading state')
-
-test.todo('renders data')
---
+++ src/useCustomHook.test.js
@@ -0,0 +1,8 @@
+import { act, renderHook } from '@testing-library/react-hooks'
+import { useCustomHook } from './useCustomHook'
+
+test.todo('custom hook return state')
+
+test.todo('custom hook with custom initial value')
+
+test.todo('custom hook with updater')
