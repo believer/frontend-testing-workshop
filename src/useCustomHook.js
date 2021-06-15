4fa91d5053d571eebef7366bbf2948010a3c1915
Rickard Natt och Dag <rickard@nattochdag.dev>
2021-06-15 21:04:22 +0200

    refactor: update final state of hooks example

--- src/useCustomHook.js
+++ src/useCustomHook.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
 export const useCustomHook = (initial = 'Initial') => {
-  const [state, setState] = React.useState(initial)
-
-  return [state, setState]
+  return React.useState(initial)
 }
